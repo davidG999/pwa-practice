94d6634d551bfccc1f025a824bbe52cc6b801208
DavidGit999 <itprog999@gmail.com>
2022-01-04 18:36:06 +0200

    Create offline page

--- sw.js
+++ sw.js
@@ -1,9 +1,11 @@
 const staticCacheName = 's-app-v3'
+const dynamicCacheName = 'd-app-v3'
 
 const assetUrls = [
   'index.html',
   '/js/app.js',
-  '/css/styles.css'
+  '/css/styles.css',
+  'offline.html'
 ]
 
 self.addEventListener('install', async event => {
@@ -16,12 +18,20 @@ self.addEventListener('activate', async event => {
   await Promise.all(
     cacheNames
       .filter(name => name !== staticCacheName)
+      .filter(name => name !== dynamicCacheName)
       .map(name => caches.delete(name))
   )
 })
 
 self.addEventListener('fetch', event => {
-  console.log('Fetch', event.request.url)
+  const { request } = event
+
+  const url = new URL(request.url)
+  if (url.origin === location.origin) {
+    event.respondWith(cacheFirst(request))
+  } else {
+    event.respondWith(networkFirst(request))
+  }
 
   event.respondWith(cacheFirst(event.request))
 })
@@ -29,4 +39,16 @@ self.addEventListener('fetch', event => {
 async function cacheFirst(request) {
   const cached = await caches.match(request)
   return cached ?? await fetch(request)
+}
+
+async function networkFirst(request) {
+  const cache = await caches.open(dynamicCacheName)
+  try {
+    const response = await fetch(request)
+    await cache.put(request, response.clone())
+    return response
+  } catch (e) {
+    const cached = await cache.match(request)
+    return cached ?? await caches.match('/offline.html')
+  }
 }
